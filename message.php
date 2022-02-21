74f1e95d8e1bd36842656c571e2c662e556321bc
Chengchao Yuan <69713042+yuan-cc@users.noreply.github.com>
2022-02-21 05:52:13 -0500

    Update message.php

--- message.php
+++ message.php
@@ -3,12 +3,7 @@
 if(isset($_POST['firstname']))
 {
 $data=$_POST['firstname'];
-$fp = fopen('message.txt', 'a');
-fwrite($fp, $data);
-  $data=$_POST['email'];
-fwrite($fp, $data);
-    $data=$_POST['subject'];
-fwrite($fp, $data);
+
 
 fclose($fp);
 }
